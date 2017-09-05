9070745914572c4750b910a2e2cc452bd23a9d46
Frank <jfrank1500@gmail.com>
2017-09-05 11:49:23 -0300

    Apos a aula do dia 02 set

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,9 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-/* -------------- MODULO HTTP --------------*/
-import { HttpModule } from '@angular/http'; 
+
+import {HttpModule} from '@angular/http';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
@@ -15,7 +16,6 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    /* -------------- MODULO HTTP --------------*/
     HttpModule,
     IonicModule.forRoot(MyApp)
   ],
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,43 +1,124 @@
-/* Declaração de componentes externos */
+
 import { Component } from '@angular/core';
-import { NavController, LoadingController } from 'ionic-angular';
+import { NavController, LoadingController, ActionSheetController } from 'ionic-angular';
 import { Http } from '@angular/http';
 import { InAppBrowser } from '@ionic-native/in-app-browser';
 import 'rxjs/add/operator/map';
 
-/* Decorador do componente page-home */
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-/* Definição da classe do componente */
 export class HomePage {
   public feeds: Array<string>;
   private url: string = "https://www.reddit.com/new.json";
+  private olderPosts: string = "https://www.reddit.com/new.json?after=";
+  private newerPosts: string = "https://www.reddit.com/new.json?before=";
+  public noFilter: Array<any>;
+  public hasFilter: boolean = false;
 
   constructor(public navCtrl: NavController,
     public http: Http,
-    public loadingCtrl: LoadingController) {
+    public loadingCtrl: LoadingController,
+    public actionSheetCtrl: ActionSheetController) {
     this.fetchContent();
   }
+
   fetchContent(): void {
     let loading = this.loadingCtrl.create({
       content: 'Buscando conteúdo...'
     });
+
     loading.present();
     this.http.get(this.url).map(res => res.json())
       .subscribe(data => {
         this.feeds = data.data.children;
         loading.dismiss();
-        this.feeds.forEach((e:any, i, a) => {
-          if (!e.data.thumbnail || e.data.thumbnail.indexOf('b.thumbs.redditmedia.com') === -1) {
-            e.data.thumbnail = 'http://www.redditstatic.com/icon.png';
-          }
-        })
+        this.consertarImagens();
+        this.noFilter = this.feeds;
+        this.hasFilter = false;
       });
   }
-  itemSelected(feed): void {
+
+  consertarImagens() {
+    this.feeds.forEach((e: any, i, a) => {
+      if (!e.data.thumbnail || e.data.thumbnail.indexOf('b.thumbs.redditmedia.com') === -1) {
+        e.data.thumbnail = 'http://www.redditstatic.com/icon.png';
+      }
+    });
+  }
+
+  itemSelected(feed: any): void {
     let browser = new InAppBrowser();
     browser.create(feed.data.url, '_system');
   }
+
+  doInfinite(infiniteScroll) {
+
+    let entradas: any = this.feeds;
+
+    let paramsUrl = (this.feeds.length > 0) ? entradas[this.feeds.length - 1].data.name : "";
+
+    this.http.get(this.olderPosts + paramsUrl).map(res => res.json())
+      .subscribe(data => {
+        this.feeds = this.feeds.concat(data.data.children);
+        this.consertarImagens();
+        infiniteScroll.complete();
+        this.noFilter = this.feeds;
+        this.hasFilter = false;
+      });
+
+  }
+
+  doRefresh(refresher) {
+
+    let entradas: any = this.feeds;
+    let paramsUrl = entradas[0].data.name;
+
+    this.http.get(this.newerPosts + paramsUrl).map(res => res.json())
+      .subscribe(data => {
+        this.feeds = data.data.children.concat(this.feeds);
+        this.consertarImagens();
+        refresher.complete();
+        this.noFilter = this.feeds;
+        this.hasFilter = false;
+      });
+  }
+
+  showFilters(): void {
+
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Filtros:',
+      buttons: [
+        {
+          text: 'Música',
+          handler: () => {
+            this.feeds = this.noFilter.filter((item) => item.data.subreddit.toLowerCase() === "music");
+            this.hasFilter = true;
+          }
+        },
+        {
+          text: 'Filmes',
+          handler: () => {
+            this.feeds = this.noFilter.filter((item) => item.data.subreddit.toLowerCase() === "movies");
+            this.hasFilter = true;
+          }
+        },
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            this.feeds = this.noFilter;
+            this.hasFilter = false;
+          }
+        }
+      ]
+    });
+
+    actionSheet.present();
+
+  }
+
+
+
 }
